b78da1112d32d694ca1219b36e719305a226840b
Yaronn44 <ivan-44@hotmail.fr>
2017-10-27 16:09:37 +0200

    Add files via upload

--- requetes.sql
+++ requetes.sql
@@ -5,6 +5,6 @@ prompt *******************************************************************
 prompt ****************************  REQUETES ****************************
 prompt *******************************************************************
 
-SELECT  EXTRACT( YEAR FROM date_deb) AS annee, date_deb, nb_grevistes, SUM(DISTINCT nb_grevistes) 
+SELECT  EXTRACT( YEAR FROM date_deb) AS annee, date_deb, SUM(DISTINCT nb_grevistes) 
 FROM Table_Faits NATURAL JOIN Temps NATURAL JOIN Nb_Travailleurs
-GROUP BY ROLLUP (EXTRACT( YEAR FROM date_deb), date_deb, nb_grevistes);
+GROUP BY ROLLUP (EXTRACT( YEAR FROM date_deb), date_deb);
