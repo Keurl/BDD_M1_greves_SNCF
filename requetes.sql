1f13eb4478fc9c97f687411c23d7ba00dc0cfe58
Yaronn44 <ivan-44@hotmail.fr>
2017-11-10 12:07:44 +0100

    Update requetes.sql

--- requetes.sql
+++ requetes.sql
@@ -1,5 +1,7 @@
 --Fichier d'insertion des requêtes
 
+SET PAGESIZE 300
+SET LINESIZE 300
 
 prompt *******************************************************************
 prompt ****************************  REQUETES ****************************
@@ -27,43 +29,44 @@ nombre total de grèvistes depuis 2011
 
 durée moyenne des grèves en 2010
 
-
-
-
-
   */
+  
+prompt ****************************  REQUETE N°1
+prompt 
 
-SELECT  EXTRACT( YEAR FROM date_deb) AS annee, date_deb, SUM(DISTINCT nb_grevistes) 
+SELECT  annee, date_deb, SUM(DISTINCT nb_grevistes) AS nb_grevistes
 FROM Table_Faits NATURAL JOIN Temps NATURAL JOIN Nb_Travailleurs
-GROUP BY ROLLUP (EXTRACT( YEAR FROM date_deb), date_deb);
+WHERE nb_grevistes IS NOT NULL
+GROUP BY ROLLUP (annee, date_deb); 
 
 
-prompt *******************************************************************
-prompt Affiche le nombre de grèviste par an et par association en utilisant grouping sets
+prompt ****************************  REQUETE N°2
+prompt Top 10 des plus grosses grèves toutes années confondu
+prompt
 
-SELECT ID_Orga, annee, SUM(DISTINCT nb_grevistes)
-FROM Table_Faits NATURAL JOIN Nb_Travailleurs NATURAL JOIN Temps NATURAL JOIN Orga
-GROUP BY GROUPING SETS((annee), (ID_Orga), ());
-
-
-prompt *******************************************************************
-prompt Affiche le nombre de grèves par saison utilisant group by rollup
-
-SELECT saison, Count(*)
-FROM Table_Faits NATURAL JOIN Temps
-GROUP BY ROLLUP (saison)
-
-/*top 10 des plus grosses grèves toutes années confondu : a tester*/
+Column Motifs format a40
+Column date_deb format a11
+Column date_fin format a11
 
 SELECT date_deb, date_fin, motifs, nb_grevistes, rank() over(order by nb_grevistes desc) AS top_10
 FROM Table_Faits NATURAL JOIN Temps NATURAL JOIN Nb_Travailleurs NATURAL JOIN Motifs
 WHERE ROWNUM <=10;
 
-/* nombre de grevistes total par an (uniquement les agregats) a tester */
+/* ROWNUM va être exécuter avant le order by ce qui ici posait soucis, avec ORACLE 12 il existe la commande FETCH qui permet de résoudre le problème*/
+
+
+prompt ****************************  REQUETE N°3
+prompt Nombre de grevistes total par an (uniquement les agregats) a tester
+prompt
 
 SELECT annee, SUM(nb_grevistes)
 FROM Table_Faits NATURAL JOIN Temps NATURAL JOIN Nb_Travailleurs
 GROUP BY GROUPING SETS (annee);
+
+
+prompt ****************************  REQUETE N°4
+prompt  % de grève de chaque type chaque année depuis 2002
+prompt
 
 /* % de grève de chaque type chaque année depuis 2002
  pour chaque année, chaque categorie faire : nombre de grèves dans cette categorie cette annee/nombre de grèves toute l'année x 100
@@ -72,3 +75,19 @@ GROUP BY GROUPING SETS (annee);
 SELECT annee, categorie_greve, ((SELECT COUNT(*) FROM Table_Faits NATURAL JOIN Motifs GROUP BY (annee,categorie_greve)) /((SELECT COUNT(*)/3 FROM Table_Faits JOIN Tally ON Tally.Table_Faits <= 3 GROUP BY annee)*100)) AS ratio_type_greve
 FROM Table_Faits NATURAL JOIN Motifs NATURAL JOIN Temps 
 GROUP BY GROUPING SETS (annee,categorie_greve);
+
+
+prompt ****************************  REQUETE N°5
+prompt Affiche le nombre de grèviste par an et par association en utilisant grouping sets
+
+SELECT ID_Orga, annee, SUM(DISTINCT nb_grevistes)
+FROM Table_Faits NATURAL JOIN Nb_Travailleurs NATURAL JOIN Temps NATURAL JOIN Orga
+GROUP BY GROUPING SETS((annee), (ID_Orga), ());
+
+
+prompt ****************************  REQUETE N°6
+prompt Affiche le nombre de grèves par saison utilisant group by rollup
+
+SELECT saison, Count(*)
+FROM Table_Faits NATURAL JOIN Temps
+GROUP BY ROLLUP (saison)
